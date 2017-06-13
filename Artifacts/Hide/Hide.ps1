695cfaee3e6afd41031cfd75fd8d0ecf10d1b289
latitude92 <jag@ses7.onmicrosoft.com>
2017-06-13 17:48:14 +0530

    hide hide

--- Artifacts/Hide/Hide.ps1
+++ Artifacts/Hide/Hide.ps1
@@ -1,3 +1,6 @@
-﻿Get-ChildItem -path "C:\"  -Force | foreach {$_.attributes = "System,Hidden,ReadOnly"}
-
-#Get-ChildItem -path "C:\"  -Force | foreach {$_.attributes = ""}
+﻿Get-ChildItem -path "C:\Bandicam"  -Force | foreach {$_.attributes = "System,Hidden,ReadOnly"} -ErrorAction Continue
+cd "C:\"
+$f=get-item .\Bandicam -Force 
+$f.attributes= "System,Hidden,ReadOnly"
+#Get-ChildItem -path "C:\Bandicam"  -Force | foreach {$_.attributes = ""}
+#$f.attributes=""
